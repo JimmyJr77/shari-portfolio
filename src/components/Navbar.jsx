f7ec2ef1c876c5554433670558f1dd8e2a898acb
jimmyjr77 <jimmyjr.obrien@gmail.com>
2025-06-22 12:26:31 -0400

    created a hamburger menu for small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
+  const toggle = () => setOpen((prev) => !prev)
+
   return (
     <nav className={styles.nav}>
       <div className={styles.inner}>
-        {/* Logo always goes home */}
         <Link to="/" className={styles.logo}>
-          Shari Arroyo-Brown
+          Arroyo-Brown
         </Link>
-        <ul className={styles.links}>
-          <li>
-            <Link to="/" className={styles.link}>Home</Link>
-          </li>
-          <li>
-            <Link to="/#services" className={styles.link}>Services</Link>
-          </li>
-          <li>
-            <Link to="/#cases" className={styles.link}>Case Studies</Link>
-          </li>
-          <li>
-            <Link to="/#contact" className={styles.link}>Contact</Link>
-          </li>
-          <li>
-            <Link to="/about" className={styles.link}>About</Link>
-          </li>
+
+        {/* Hamburger button */}
+        <button
+          className={styles.hamburger}
+          aria-label="Toggle menu"
+          onClick={toggle}
+        >
+          <span className={styles.bar} />
+          <span className={styles.bar} />
+          <span className={styles.bar} />
+        </button>
+
+        <ul
+          className={`${styles.links} ${open ? styles.open : ''}`}
+          onClick={() => setOpen(false)}
+        >
+          <li><Link to="/"       className={styles.link}>Home</Link></li>
+          <li><Link to="/#services" className={styles.link}>Services</Link></li>
+          <li><Link to="/#cases"    className={styles.link}>Case Studies</Link></li>
+          <li><Link to="/#contact"  className={styles.link}>Contact</Link></li>
+          <li><Link to="/about"     className={styles.link}>About</Link></li>
         </ul>
       </div>
     </nav>
